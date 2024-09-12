13fced737f381078763b026f3d5736d79438cdbe
ChinmayAshtankar <chinmayashtankar57@gmail.com>
2024-09-13 01:09:39 +0530

    Update task_2.cpp

--- task_2.cpp
+++ task_2.cpp
@@ -1,47 +1,57 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
 int main()
 {
-    cout<<"calculator\n";
-
-    int op1 , op2;
-    cout<<"Enter operand 1 : "; cin>>op1;
-    cout<<"Enter operand 2 : "; cin>>op2;
-
-
-    cout<<"Choose an operation : \n";
-    cout<<"1. Addition\n";
-    cout<<"2. Subtraction\n";
-    cout<<"3. Multiplication\n";
-    cout<<"4. Division\n";
-    
     int choice;
-    cout<<"Enter choice : ";
-    cout<<"Result : ";
-    cin>>choice;
-    switch(choice)
+    do
     {
-        case 1 :
-        {
-            cout<<op1 + op2;
-            break;
-        }
-        case 2 :
-        {
-            cout<<op1 - op2;
-            break;
-        }
-        case 3 :
-        {
-            cout<<op1 * op2;
-            break;
-        }
-        case 4 :
-        {
-            cout<<op1 / op2;
-            break;
-        }
-    }
-    return 0;
+        int op1, op2;
+        cout << "Enter operand 1: "; 
+        cin >> op1;
+        cout << "Enter operand 2: "; 
+        cin >> op2;
 
+        cout << "Choose an operation:\n";
+        cout << "1. Addition\n";
+        cout << "2. Subtraction\n";
+        cout << "3. Multiplication\n";
+        cout << "4. Division\n";
+        cout << "5. Exit\n"; 
+
+        cout << "Enter choice: ";
+        cin >> choice;
+
+        if (choice == 5) 
+        {
+            cout << "Exit \n";
+            break; 
+        }
+
+        cout << "Result: ";
+
+        switch (choice)
+        {
+            case 1:
+                cout << op1 + op2;
+                break;
+            case 2:
+                cout << op1 - op2;
+                break;
+            case 3:
+                cout << op1 * op2;
+                break;
+            case 4:
+                if (op2 != 0)
+                    cout << op1 / op2;
+                else
+                    cout << "Division by zero";
+                break;
+            
+        }
+      cout <<endl;   
+    }
+     
+    while (true); 
+    return 0;
 }
